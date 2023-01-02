8333085bec37537f4abf829b6cac9fbef8249858
Zhu Guodong <zhuguodong0001@163.com>
2023-01-03 01:33:44 +0800

    reorganize project source file hierarchy

---
+++ src/common/cl_runtime.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zgd on 1/3/23.
+//
+
+#include "cl_runtime.h"
---
+++ src/common/cl_runtime.h
@@ -0,0 +1,14 @@
+//
+// Created by zgd on 1/3/23.
+//
+
+#ifndef CLBENCH_SRC_CL_RUNTIME_H_
+#define CLBENCH_SRC_CL_RUNTIME_H_
+
+#include "CL/opencl.hpp"
+
+class CLRuntime {
+
+};
+
+#endif //CLBENCH_SRC_CL_RUNTIME_H_
